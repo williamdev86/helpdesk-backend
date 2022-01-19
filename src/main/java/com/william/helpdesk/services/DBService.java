4d22da432c58b494bafb312c58742ff67498c292
williamdev86 <williamsoares86@gmail.com>
2022-01-18 21:34:47 -0300

    perfil de desenvolvimento

--- src/main/java/com/william/helpdesk/services/DBService.java
+++ src/main/java/com/william/helpdesk/services/DBService.java
@@ -33,12 +33,36 @@ public class DBService {
 		Tecnico tec1 = new Tecnico(null, "William Soares", "331.546.458-88", "willliam@gmail.com","123" );
 		tec1.addPerfil(Perfil.ADMIN);
 		
-		Cliente cli1 = new Cliente(null, "Vangerlandio Reinaldo","376.386.100-91", "vangerlandio@gmail.com", "123");
+		Tecnico tec2 = new Tecnico(null, "Carlos Magno", "182.642.550-00", "carlos@gmail.com","123" );
+		tec2.addPerfil(Perfil.ADMIN);
 		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01","Primeiro chamado", tec1, cli1);
+		Tecnico tec3 = new Tecnico(null, "Jackson Martins", "421.458.190-37", "jackson@gmail.com","123" );
+		tec3.addPerfil(Perfil.ADMIN);
 		
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
+		Tecnico tec4 = new Tecnico(null, "Alberto Sales", "098.323.810-37", "alberto@gmail.com","123" );
+		tec4.addPerfil(Perfil.ADMIN);
+		
+		Tecnico tec5 = new Tecnico(null, "Sergio Junior", "328.945.970-53", "sergio@gmail.com","123" );
+		tec5.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Sales Albino","376.386.100-91", "sales@gmail.com", "123");
+		
+		Cliente cli2 = new Cliente(null, "Orion Moreira","647.860.080-79", "orion@gmail.com", "123");
+		
+		Cliente cli3 = new Cliente(null, "Thalis Vieira","188.808.470-74", "thalis@gmail.com", "123");
+		
+		Cliente cli4 = new Cliente(null, "Marcelo Santos","083.910.960-17", "marcelo@gmail.com", "123");
+
+		Cliente cli5 = new Cliente(null, " Maicon Cruz","708.908.590-27", "maicon@gmail.com", "123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01","Primeiro chamado", tec2, cli3);
+		
+		Chamado c2 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 02","Segundo chamado", tec4, cli2);
+		
+		Chamado c3 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 03","Terceiro chamado", tec3, cli4);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3, cli4, cli5));
+		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 }
