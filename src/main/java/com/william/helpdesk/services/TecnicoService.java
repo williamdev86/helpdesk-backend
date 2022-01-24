5b97d53355ce4f1edf8bf6324258e6948c84a4ca
williamdev86 <williamsoares86@gmail.com>
2022-01-24 12:12:12 -0300

    metodo PUT para Técnico implementado

--- src/main/java/com/william/helpdesk/services/TecnicoService.java
+++ src/main/java/com/william/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.william.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,6 +40,14 @@ public class TecnicoService {
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
 	}
+	
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCPFEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
 
 	private void validaPorCPFEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -51,4 +61,5 @@ public class TecnicoService {
 		}
 		
 	}
+
 }
